52f6bae39a30a551b4400dc793b2f8ff8dac3999
golam habib palash <golamhabibpalash@hotmail.com>
2021-12-29 01:49:09 +0600

    Event Model Added

--- EMS.Entity/Event.cs
+++ EMS.Entity/Event.cs
@@ -8,5 +8,10 @@ namespace EMS.Entity
 {
     public class Event
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
     }
 }
