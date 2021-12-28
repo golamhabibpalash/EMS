f3d7c10bade7620af816983ad1b394cfa41e48a0
golam habib palash <golamhabibpalash@hotmail.com>
2021-12-29 01:46:50 +0600

    Repository added

---
+++ EMS.DB/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EMS.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.DB
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
