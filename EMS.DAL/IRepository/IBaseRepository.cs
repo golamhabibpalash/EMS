3f73ec366f0a86e4b041a4074a97502e39d00177
golam habib palash <golamhabibpalash@hotmail.com>
2021-12-29 02:22:11 +0600

    Base Repository modified

--- EMS.DAL/IRepository/IBaseRepository.cs
+++ EMS.DAL/IRepository/IBaseRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace EMS.DAL.IRepository
 {
-    public interface IBaseRepository
+    public interface IBaseRepository<T> where T:class
     {
+        Task<bool> AddAsync(T entity);
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> RemoveAsync(T entity);
     }
 }
--- EMS.DAL/Repository/BaseRepository.cs
+++ EMS.DAL/Repository/BaseRepository.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EMS.DAL.IRepository;
+using EMS.DB;
+using Microsoft.EntityFrameworkCore;
 
 namespace EMS.DAL.Repository
 {
-    public class BaseRepository
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
+        public readonly ApplicationDbContext _context;
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public DbSet<T> Table { get { return _context.Set<T>(); } }
+        public async Task<bool> AddAsync(T entity)
+        {
+            Table.Add(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> RemoveAsync(T entity)
+        {
+            Table.Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await Table.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await Table.FirstAsync();
+        }
+
+        public async Task<bool> UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            return await _context.SaveChangesAsync() > 0;
+        }
     }
 }
