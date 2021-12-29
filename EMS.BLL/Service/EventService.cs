d3a5f324723e3e3bdd511e566eec6291956ff828
golam habib palash <golamhabibpalash@hotmail.com>
2021-12-30 00:12:28 +0600

    Event Repository and Interface added

---
+++ EMS.BLL/Service/EventService.cs
@@ -0,0 +1,19 @@
+﻿using EMS.BLL.IService;
+using EMS.DAL.IRepository;
+using EMS.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.BLL.Service
+{
+    public class EventService : BaseService<Event>, IEventService
+    {
+        public EventService(IEventRepository eventRepository):base(eventRepository)
+        {
+
+        }
+    }
+}
